47da932c0202c85096ec973c8e4a3a1ebb466b7a
Loumann <Bleyka80@gmail.com>
2022-12-02 16:41:46 +0500

    добавил регулярку

--- DataProvider/Program.cs
+++ DataProvider/Program.cs
@@ -4,12 +4,14 @@ using System.IO.Ports;
 using System.Net;
 using System.Net.Http.Json;
 using System.Text;
+using System.Text.RegularExpressions;
 using Newtonsoft.Json;
 
 class Programs 
 {
     private static SerialPort? _port; 
-    
+    const string pattern = @"^\s+|^\w+\s+";
+    const string substitution = @"";
     static bool _continue;
     
     private static readonly HttpClient client = new HttpClient();
@@ -27,22 +29,59 @@ class Programs
 
         while (_continue)
         {
-            Read();
+            ReadSerial();
         }
 
         while (_continue)
         {
             var message = Console.ReadLine();
-
-            if (stringComparer.Equals("quit", message))
+            if (stringComparer.Equals("test",message))
+            {
+                ReadTest();
+            }
+            else if(stringComparer.Equals("quit", message))
             {
                 _continue = false;
             }
         }
         _port.Close();
     }
-    
-    private static void Read()
+
+    private static void ReadSerial()
+    {
+        var str = _port?.ReadLine();
+        var currentString = str.Replace("*", "");
+        
+        var options = RegexOptions.Multiline;
+        var regex = new Regex(pattern, options);
+
+        currentString = regex.Replace(currentString, substitution);
+
+        var stringArray = currentString.Split("\n");
+        var counter = 0;
+
+        var values = new string[10];
+
+        //if (stringArray.Length != 16) Console.WriteLine("Not correct data");
+      
+
+        for (var i = 5; i < 15; i++)
+        {
+            values[counter++] = stringArray[i];
+        }
+
+        currentString.Remove(0);
+        currentString = string.Join("\n", values);
+
+        currentString = regex.Replace(currentString, substitution);
+        currentString = currentString.Replace("\r", "");
+        //stringArray = currentString.Split("\n");
+
+        var httpClient = new HttpClient();
+        var result = httpClient.PostAsync("http://localhost:8081/userPost",new StringContent(currentString,Encoding.UTF8,"application/json")).Result;
+    }
+
+    private static void ReadTest()
     {
         var buffer = _port?.ReadLine();
 
@@ -52,5 +91,7 @@ class Programs
         var httpClient = new HttpClient();
 
         var result = httpClient.PostAsync("http://localhost:8081/userPost",new StringContent(json.ToString(),Encoding.UTF8,"application/json")).Result;
+
+        Console.WriteLine(result);
     }
 }
